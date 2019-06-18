41c292dfdaf0ee68cb3ed87fd00f8e50015e13e7
Éli Marshal <elimarshal@gmail.com>
2019-06-18 17:46:09 -0600

    Fix script to work if directory does not exist

--- resources/DisableFirefoxUpdates.bat
+++ resources/DisableFirefoxUpdates.bat
@@ -1,1 +1,2 @@
-copy %~dp0policies.json  "C:\Program Files\Mozilla Firefox\distribution"
+if not exist "C:\Program Files\Mozilla Firefox\distribution\" mkdir "C:\Program Files\Mozilla Firefox\distribution\"
+copy %~dp0policies.json  "C:\Program Files\Mozilla Firefox\distribution\policies.json"

10c2a7010a676d8473b7755b363e99f1b721e33e
Éli Marshal <835958+EMarshal@users.noreply.github.com>
2025-04-04 10:21:00 -0600

    Remove Firefox update policies

--- resources/DisableFirefoxUpdates.bat
+++
@@ -1,2 +0,0 @@
-if not exist "C:\Program Files\Mozilla Firefox\distribution\" mkdir "C:\Program Files\Mozilla Firefox\distribution\"
-copy %~dp0policies.json  "C:\Program Files\Mozilla Firefox\distribution\policies.json"
